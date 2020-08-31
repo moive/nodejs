c7e63122f6299744c246782259ed75e6bad17cce
Moises Velasquez <mvelasquez@pictoric.com>
2020-08-30 22:52:33 -0500

    working with files epj

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//motor de plantillas
+app.set('view engine', 'ejs');
+app.set('Views', __dirname + '/views');
+
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.send("Mi respuesta desde el servidor express v.2");
+    res.render("index", {title: "Home | site nodejs"});
 });
 
 app.get("/servicios", (req, res)=>{
-    res.send("Estás en la vista de servicio.");
+    res.render("servicios", {title: "Mensaje dinámico desde servicios."});
 });
 
 app.use((req, res, next)=>{
-    res.status(404).sendFile(__dirname + "/public/404.html");
+    res.status(404).render("404", {title:"404"});
 });
 
 app.listen(port, ()=>{
